f328135eac220e5b789b54e38cc5f237af951dab
Ruramai <ruramaimuchenga@gmail.com>
2024-08-24 18:17:02 +0200

    latest version

---
+++ import pandas as pd.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
